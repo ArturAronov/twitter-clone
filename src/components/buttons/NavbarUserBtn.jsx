bb8be798466e460888ac2daeb6ccff50e826a554
ArturAronov <unionco@icloud.com>
2022-09-05 16:53:09 +0800

    fixed bug that didnt mutate state on logout and sign up

--- src/components/buttons/NavbarUserBtn.jsx
+++ src/components/buttons/NavbarUserBtn.jsx
@@ -2,18 +2,58 @@ import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import axios from 'axios'
 
-import userProfile from '../../hooks/useProfile'
+import useProfile from '../../hooks/useProfile'
 import UserStats from '../profile/UserStats'
 
 const NavbarUserBtn = () => {
   const router = useRouter()
-  const { profile, newProfile } = userProfile()
+  const { profile, newProfile } = useProfile()
+  const [ authMenu, setAuthMenu] = useState()
 
-  const handleLogOut = async () => {
-    await axios.delete('/api/auth/logout')
-    await newProfile('/api/my/profile')
-    await router.push('/')
+  const handleLogOut = () => {
+    axios.delete('/api/auth/logout')
+        .then(() => router.push('/'))
+        .then(() => newProfile('/api/my/profile', profile))
+        .catch((err) => console.log(err))
   }
+
+  useEffect(() => {
+    if(profile?.id){
+      setAuthMenu(
+        <div
+          className="hover:bg-zinc-800 px-2 py-3 cursor-pointer"
+          onClick={() => {
+            handleLogOut();
+          }}
+        >
+          Log Out
+        </div>
+      )
+    } else {
+      setAuthMenu(
+        <div>
+          <div
+            className="hover:bg-zinc-800 px-2 py-3 cursor-pointer modal-action"
+            onClick={() => {
+              router.push('/auth/signup')
+              newProfile()
+            }}
+          >
+            Sign Up
+          </div>
+          <div
+            className="hover:bg-zinc-800 px-2 py-3 cursor-pointer"
+            onClick={() => {
+              router.push('/auth/login')
+              newProfile()
+            }}
+          >
+            Log In
+          </div>
+        </div>
+      )
+    }
+  }, [profile])
 
   return (
     <div className="dropdown sm:dropdown-right sm:dropdown-top sm:hero-content sm:mb-5 bg-zinc-1000">
@@ -24,61 +64,23 @@ const NavbarUserBtn = () => {
       </label>
       <div tabIndex="0" className="dropdown-content menu p-0 m-0 bg-base-100 shadow-zinc-500 shadow-md sm:border border-zinc-700 w-40 top-0 -left-3 sm:h-content sm:w-content sm:rounded-md p-2 h-screen sm:h-min">
         {
-              profile?.id
-              && <div> <UserStats /> </div>
-            }
+          profile?.id
+          && <div> <UserStats /> </div>
+        }
         <div className="flex flex-col justify-between sm:justify-start mt-5">
           {
-                profile?.id
-                && (
-                <div
-                  className="hover:bg-zinc-800 px-2 py-3 cursor-pointer"
-                  onClick={() => router.push('/settings')}
-                >
-                  Settings
-                </div>
-                )
-}
+            profile?.id
+            && (
+            <div
+              className="hover:bg-zinc-800 px-2 py-3 cursor-pointer"
+              onClick={() => router.push('/settings')}
+            >
+              Settings
+            </div>
+            )
+          }
           <label>
-            {
-                  profile?.id
-                  && (
-                  <div
-                    className="hover:bg-zinc-800 px-2 py-3 cursor-pointer"
-                    onClick={handleLogOut}
-                  >
-                    Log Out
-                  </div>
-                  )
-                }
-            {
-                  !profile?.id
-                  && (
-                  <label
-                    className="hover:bg-zinc-800 px-2 py-3 cursor-pointer modal-action"
-                    onClick={() => {
-                      router.push('/auth/signup')
-                      newProfile('/api/my/profile')
-                    }}
-                  >
-                    Sign Up
-                  </label>
-                  )
-                }
-            {
-                  !profile?.id
-                  && (
-                  <div
-                    className="hover:bg-zinc-800 px-2 py-3 cursor-pointer"
-                    onClick={() => {
-                      router.push('/auth/login')
-                      newProfile('/api/my/profile')
-                    }}
-                  >
-                    Log In
-                  </div>
-                  )
-                }
+            { authMenu }
           </label>
         </div>
       </div>
