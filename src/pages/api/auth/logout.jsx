f6894fd4921c1ca8a3100ed21d374b86241529c4
ArturAronov <unionco@icloud.com>
2022-09-05 16:54:44 +0800

    updated success message

--- src/pages/api/auth/logout.jsx
+++ src/pages/api/auth/logout.jsx
@@ -7,7 +7,7 @@ handler
   .use(auth)
   .delete(async (req, res) => {
     req.session.destroy()
-    res.status(201).json({"msg": "successfully logged out"})
+    res.status(201).json({ success: true, message: 'successfully logged out' })
   })
 
 export default handler
