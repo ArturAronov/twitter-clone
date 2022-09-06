1a3913a1c12245ff4754ed2aeaffbf8ceff1a61d
ArturAronov <unionco@icloud.com>
2022-09-07 01:07:36 +0800

    added API that retrieves user interactions

---
+++ src/pages/api/my/follows-likes-bookmarks.jsx
@@ -0,0 +1,27 @@
+import nextConnect from 'next-connect'
+import prisma from '../../../controllers/_helpers/prisma'
+import handleErrors from '../../../controllers/_helpers/handleErrors'
+import auth from '../../../controllers/_middlewares/auth'
+
+const handler = nextConnect()
+
+handler
+  .use(auth)
+  .get(async (req, res) => {
+    try {
+      const getFollowing = await prisma.interaction.findMany({
+        where: {
+          actionType: {
+            in: ['FOLLOW', 'BOOKMARK', 'LIKE']
+          },
+          postUserId: req.session.user.id
+        }
+
+      })
+
+      return res.status(201).json(getFollowing)
+    } catch (err) {
+      return handleErrors(res, err)
+    }
+  })
+export default handler
