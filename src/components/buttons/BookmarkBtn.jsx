22575cdc60dacadf3b5d0ca8e52a06898cce6897
ArturAronov <unionco@icloud.com>
2022-09-09 01:16:22 +0800

    finalised bookmark button component

--- src/components/buttons/BookmarkBtn.jsx
+++ src/components/buttons/BookmarkBtn.jsx
@@ -1,53 +1,77 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+import useProfile from '../../hooks/useProfile'
 import useInteractions from '../../hooks/useInteractions'
+
 import RemoveBookmark from './RemoveBookmark'
 import AddBookmark from './AddBookmark'
 
 const BookmarkBtn = (props) => {
   const { interactions, newInteraction } = useInteractions()
+  const { profile } = useProfile()
 
-  const [button, setButton] = useState()
-  const [bookmarks, setBookmarks] = useState([])
+  const [interaction, setInteraction] = useState([])
+  const [buttonActive, setButtonActive] = useState(false)
 
-  const toggleInteraction = async (postId, interaction) => {
-    const data = {
-      actionType: interaction,
-      postId
+  const toggleInteraction = async () => {
+    let data = {
+      postUserId: profile.id,
+      actionType: 'BOOKMARK',
     }
+
+    if(props?.post?.post?.userId) {
+      // Since the data feed from Likes component is different from the other components, there the interactionUserId needs to come from a different source
+      data = {
+        ...data,
+        interactionUserId: props.post.post.userId,
+        postId: props.post.postId,
+      }
+    } else {
+      data = {
+        ...data,
+        interactionUserId: props.post.userId,
+        postId: props.post.id,
+      }
+    }
+
     await axios.put('/api/my/interaction', data)
     await newInteraction()
   }
 
-  const bookmark = () => {
-    setBookmarks(interactions.filter((element) => element.actionType === 'BOOKMARK' && element.postId === props.id))
-  }
-
   const getData = async () => {
-    const data = await axios.get('/api/my/interactions').then((res) => res.data)
-
-    await setBookmarks(data.filter((element) => element.actionType === 'BOOKMARK' && element.postId === props.id))
-
-    return data
+    if(interactions && props?.post?.actionType) {
+      setInteraction(interactions.filter(element => {
+        return element.actionType === 'BOOKMARK' && element.postId === props.post.postId
+      }))
+    } else if(interactions && props?.post?.postType) {
+      setInteraction(interactions.filter(element => {
+        return element.actionType === 'BOOKMARK' && element.postId === props.post.id
+      }))
+    }
   }
+
   useEffect(() => {
     getData()
-  }, [interactions])
+  },[])
 
   useEffect(() => {
-    bookmarks.length === 1 ? setButton(<RemoveBookmark />) : setButton(<AddBookmark />)
-  }, [bookmarks, interactions])
+    getData()
+  },[interactions])
+
+  useEffect(() => {
+    interaction.length === 1 ? setButtonActive(<RemoveBookmark />) : setButtonActive(<AddBookmark />)
+  }, [profile, interaction])
 
   return (
     <div
       className="cursor-pointer"
       onClick={async () => {
-        await toggleInteraction(props.id, 'BOOKMARK')
-        await bookmark()
+        await toggleInteraction()
+        await newInteraction()
       }}
     >
-      {button}
+      {buttonActive}
     </div>
   )
 }
