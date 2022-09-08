621f7e59cf66bf2dc74553241454391fa6d69169
ArturAronov <unionco@icloud.com>
2022-09-09 01:15:31 +0800

    completed bookmarks page

---
+++ src/pages/bookmarks/index.jsx
@@ -0,0 +1,23 @@
+import useBookmarks from '../../hooks/useBookmarks'
+import TweetFull from '../../components/tweet/TweetFull'
+
+const Bookmarks = () => {
+  const { bookmarks } = useBookmarks()
+
+  return(
+    <>
+      { bookmarks &&
+        bookmarks.map(element =>{
+          return (
+            <div key={element.id}>
+              <TweetFull tweet={element.post} />
+            </div>
+          )
+        })
+      }
+    </>
+  )
+};
+
+
+export default Bookmarks;
