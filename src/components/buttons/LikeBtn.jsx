2223cedb04bf4ba94a48249aa75ede51cd878450
ArturAronov <unionco@icloud.com>
2022-09-08 18:03:45 +0800

    fixed bugs

--- src/components/buttons/LikeBtn.jsx
+++ src/components/buttons/LikeBtn.jsx
@@ -1,56 +1,82 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import useInteractions from '../../hooks/useInteractions'
 
 import LikeBtnSVG from './LikeBtnSVG'
 
+import useInteractions from '../../hooks/useInteractions'
+import useProfile from '../../hooks/useProfile'
+
 const LikeBtn = (props) => {
   const { interactions, newInteraction } = useInteractions()
+  const { profile } = useProfile()
+  const [interaction, setInteraction] = useState([])
 
-  const [buttonActive, setButtonActive] = useState()
-  const [likes, setLikes] = useState([])
+  const [buttonActive, setButtonActive] = useState(false)
 
-  const toggleInteraction = async (postId, interaction) => {
-    const data = {
-      actionType: interaction,
-      postId
+  const toggleInteraction = async () => {
+    let data = {
+      postUserId: profile.id,
+      postId: props.post.id,
+      actionType: 'LIKE',
     }
+
+    if(props?.post?.post?.userId) {
+      // Since the data feed from Likes component is different from the other components, there the interactionUserId needs to come from a different source
+      data = {
+        ...data,
+        interactionUserId: props.post.post.userId,
+      }
+    } else {
+      data = {
+        ...data,
+        interactionUserId: props.post.userId,
+      }
+    }
+
     await axios.put('/api/my/interaction', data)
     await newInteraction()
   }
 
-  const like = () => {
-    setLikes(interactions.filter((element) => element.actionType === 'LIKE' && element.postId === props.id))
-  }
-
   const getData = async () => {
-    const data = await axios.get('/api/my/interactions').then((res) => res.data)
-
-    setLikes(data.filter((element) => element.actionType === 'LIKE' && element.postId === props.id))
-
-    return data
+    if(interactions && props?.post?.actionType) {
+      setInteraction(interactions.filter(element => {
+        return element.actionType === 'LIKE' && element.postId === props.post.postId
+      }))
+    } else if(interactions && props?.post?.postType) {
+      setInteraction(interactions.filter(element => {
+        return element.actionType === 'LIKE' && element.postId === props.post.id
+      }))
+    }
   }
+
   useEffect(() => {
     getData()
-  }, [interactions])
+  },[])
 
   useEffect(() => {
-    setButtonActive(likes.length === 1)
-  }, [likes, interactions])
+    getData()
+  },[interactions])
+
+
+  useEffect(() => {
+    if (interaction.length > 0) {
+      setButtonActive(true)
+    } else {
+      setButtonActive(false)
+    }
+  }, [profile, interaction])
 
   return (
     <div
       className="cursor-pointer"
       onClick={async () => {
-        await toggleInteraction(props.id, 'LIKE')
-        await like()
+        await toggleInteraction()
+        await newInteraction()
       }}
     >
       <LikeBtnSVG like={buttonActive} />
     </div>
   )
 }
-
-// likePosts.length ? <LikeBtn like={true} /> : <LikeBtn like={false} />
 
 export default LikeBtn
