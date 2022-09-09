b3dc34c7d0252f07eeb1e692485d150a65c32a67
ArturAronov <unionco@icloud.com>
2022-09-09 16:16:49 +0800

    changed close button, plus some other minor fixes

--- src/components/modals/TweetModal.jsx
+++ src/components/modals/TweetModal.jsx
@@ -1,34 +1,37 @@
 import NewTweet from '../tweet/NewTweet'
 import TinyRetweet from '../tweet/TinyRetweet'
+import CloseBtn from '../buttons/CloseBtn'
 
 const TweetModal = (props) => (
   <>
     <input className="modal-toggle" type="checkbox" id="TweetModal" />
-    <div className="modal">
-      <div className="flex flex-col bg-zinc-1000 border border-zinc-700 shadow-sm shadow-zinc-500 rounded-md w-1/2">
+    <div className="modal" htmlFor="TweetModal">
+      <label className="flex flex-col bg-zinc-1000 border border-zinc-700 shadow-sm shadow-zinc-500 rounded-md w-1/2">
         <div className="modal-action">
-          <label className="btn hover:bg-zinc-800" htmlFor="TweetModal">x</label>
+          <label className="btn hover:bg-zinc-800 btn btn-sm btn-circle mx-4 my-2 text-2xl text-zinc-0" htmlFor="TweetModal">
+            <CloseBtn />
+          </label>
         </div>
         {
             props.type === 'REPLY'
             && (
             <div className="px-6">
-              <TinyRetweet postId={props.post.id} />
+              <TinyRetweet postId={props.post.id} reply={true}/>
               <div className="flex flex-row px-7 py-2">
                 <div className="text-zinc-500">
                   Replying to
                 </div>
                 <div className="text-sky-600 pl-1">
-                  @{props.post.user.userName}
+                  @{props.user.userName}
                 </div>
               </div>
             </div>
             )
           }
-        <div className="modal-box">
+        <label className="modal-box">
           <NewTweet postType={props.type} post={props.post} user={props.post.user || props.user} />
-        </div>
-      </div>
+        </label>
+      </label>
     </div>
   </>
 )
