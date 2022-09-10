8cf4466b0c5e5b4b233e80e1c1bf3e823fbb380d
ArturAronov <unionco@icloud.com>
2022-09-10 18:13:02 +0800

    moved out of the individual folders into the component folder

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+const Sidebar = () => (
+  <div className="relative flex justify-center overflow-hidden">
+    <div className="hidden lg:inline flex flex-col fixed min-h-screen w-content">
+      <div className="h-content">
+      </div>
+    </div>
+  </div>
+)
+
+export default Sidebar
