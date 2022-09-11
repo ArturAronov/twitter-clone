babba071f27d50ae990395d4a17367f27cd5f376
ArturAronov <unionco@icloud.com>
2022-09-11 14:20:35 +0800

    populated home page content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import TweetSmall from './tweet/TweetSmall'
+import NewTweet from './tweet/NewTweet'
 
 const Content = () => {
+  const [tweets, setTweets] = useState([])
 
+  useEffect(() => {
+    axios.get('/api/all/posts').then(res => setTweets(res.data))
+  }, [])
 
   return(
     <>
-
+      <NewTweet />
+      <hr/>
+      {
+        tweets.map(element => {
+          return(
+            <TweetSmall tweetData={element} userData={element.user} />
+          )
+        })
+      }
     </>
   )
 }
