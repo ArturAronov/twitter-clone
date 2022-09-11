4440ac17dd873acfebc73de817fddd2d25423517
ArturAronov <unionco@icloud.com>
2022-09-11 14:20:17 +0800

    resized grids

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,11 +7,11 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <div className="grid grid-rows-12 sm:grid-cols-12 grid-flow-row inline ">
       <div className="flex justify-center sm:col-span-1 order-last sm:order-first row-span-1  ">
-        <div className="fixed sm:sticky left-0 bottom-0 sm:top-0 z-10 outline-1 outline-zinc-700 outline w-screen">
+        <div className="fixed sm:sticky left-0 bottom-0 sm:top-0 z-10 outline-1 outline-zinc-700 outline w-screen z-20">
           <UserNav />
         </div>
       </div>
-      <div className="lg:col-span-8 lg:col-span-8 col-span-10 row-span-11 break-all  relative z-0 mb-14">
+      <div className="md:col-span-7 col-span-10 row-span-11 break-all  relative z-10 mb-14 mx-1">
         <div className="sticky top-0 z-10">
           <Header />
         </div>
@@ -19,7 +19,7 @@ export default function MyApp({ Component, pageProps }) {
           <Component {...pageProps} />
         </div>
       </div>
-      <div className="lg:col-span-3 col-span-1 hidden sm:inline break-all w-content outline-1 outline-zinc-700 outline sticky right-0 top-0">
+      <div className="md:col-span-4 col-span-1 hidden sm:inline break-all w-content outline-1 outline-zinc-700 outline sticky right-0 top-0">
         <Sidebar />
       </div>
     </div>
