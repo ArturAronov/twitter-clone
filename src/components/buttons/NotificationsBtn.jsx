83518b26b3e6e4acd4b00bc00189085baf4c2d30
ArturAronov <unionco@icloud.com>
2022-09-13 12:05:39 +0800

    fixed issue with floating notification count badge

--- src/components/buttons/NotificationsBtn.jsx
+++ src/components/buttons/NotificationsBtn.jsx
@@ -8,11 +8,10 @@ import NotificationsBtnActive from './NotificationsBtnActive'
 
 const NotificationsBtn = () => {
   const { asPath } = useRouter()
-  const { notifications, newNotifications } = useNotifications()
+  const { notifications } = useNotifications()
   const [notificationsCount, setNotificationsCount] = useState(0)
 
   useEffect(() => {
-    // newNotifications()
     if(notifications) {
       const count = notifications.filter(element => element.received === false).length
       setNotificationsCount(count)
@@ -20,16 +19,18 @@ const NotificationsBtn = () => {
   }, [notifications])
 
   return(
-    <div className='relative'>
-      <span>
-        {asPath === '/notifications' ? <NotificationsBtnActive /> : <NotificationsBtnInactive />}
-      </span>
-      {
-        notificationsCount > 0 &&
-        <span className='h-[15px] min-w-[15px] absolute top-1 right-9 bg-sky-500 rounded-full text-xs text-center text-zinc-0 px-1'>
-          {notificationsCount}
+    <div className='w-full flex justify-center'>
+      <div className='relative w-11 h-full'>
+        <span>
+          {asPath === '/notifications' ? <NotificationsBtnActive /> : <NotificationsBtnInactive />}
         </span>
-      }
+        {
+          notificationsCount > 0 &&
+          <span className='h-[15px] min-w-[15px] absolute top-3 right-1 bg-sky-500 rounded-full text-xs text-center text-zinc-0 px-1'>
+            {notificationsCount}
+          </span>
+        }
+      </div>
     </div>
   )
 }
