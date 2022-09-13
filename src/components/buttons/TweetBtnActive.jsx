143aadadfd9c13a8706eed062e0704a904160ba2
ArturAronov <unionco@icloud.com>
2022-09-13 17:14:31 +0800

    bug fix

--- src/components/buttons/TweetBtnActive.jsx
+++ src/components/buttons/TweetBtnActive.jsx
@@ -1,7 +1,7 @@
 import BTN_CLASS from '../../globalVars/btn_class'
 
 const TweetBtnActive = (props) => (
-  <label className={BTN_CLASS.secondary} htmlFor="TweetModal"> { props.action } </label>
+  <label className={BTN_CLASS.secondary}> { props.action } </label>
 )
 
 export default TweetBtnActive
