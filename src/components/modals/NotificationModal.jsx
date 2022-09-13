19c3169045d028aec8aece2958db1c23e525314c
ArturAronov <unionco@icloud.com>
2022-09-13 13:52:40 +0800

    added condition that only displays modal when user is logged in

--- src/components/modals/NotificationModal.jsx
+++ src/components/modals/NotificationModal.jsx
@@ -1,56 +1,65 @@
 import router from 'next/router'
 import axios from 'axios'
+
 import useNotifications from '../../hooks/useNotifications'
+import useProfile from '../../hooks/useProfile'
+
 import BTN_CLASS from '../../globalVars/btn_class'
 
 const NotificationModal = (props) => {
   const { newNotifications } = useNotifications()
   const data = { id: parseInt(props.id) }
+  const { profile } = useProfile()
 
   return (
-    <div>
-      <input className="modal-toggle" type="checkbox" id="NotificationModal" />
-      <div className="modal">
-        <div className="modal-box border border-zinc-700 bg-zinc-1000 rounded-lg p-3">
-          <div className="flex flex-row">
-            <div className="text-sky-600 pr-1" onClick={() => router.push(`${props.content.split(' ')[0]}`)}>
-              @{props.content.split(' ')[0]}
+    <>
+      {
+        profile?.id &&
+        <div>
+          <input className="modal-toggle" type="checkbox" id="NotificationModal" />
+          <div className="modal">
+            <div className="modal-box border border-zinc-700 bg-zinc-1000 rounded-lg p-3">
+              <div className="flex flex-row">
+                <div className="text-sky-600 pr-1" onClick={() => router.push(`${props.content.split(' ')[0]}`)}>
+                  @{props.content.split(' ')[0]}
+                </div>
+                <div>
+                  {props.content.split(' ').slice(1, props.content.length - 1).join(' ')}
+                </div>
+              </div>
+              {
+                props?.postId &&
+                <div className="modal-action hero-content p-1 mt-3">
+                  <label
+                    className={BTN_CLASS.primary}
+                    htmlFor="NotificationModal"
+                    onClick={async () => {
+                      await axios.put('/api/my/notifications/read', data)
+                      await newNotifications()
+                      await router.push(`/tweet/${props.postId}`)
+                    }}
+                  >
+                    View Post
+                  </label>
+                </div>
+              }
+              <div className="modal-action hero-content">
+                <label
+                  className={BTN_CLASS.primary}
+                  htmlFor="NotificationModal"
+                  onClick={async () => {
+                    await axios.put('/api/my/notifications/read', data)
+                    await newNotifications()
+                  }}
+                >
+                  Close
+                </label>
+              </div>
             </div>
-            <div>
-              {props.content.split(' ').slice(1, props.content.length - 1).join(' ')}
-            </div>
-          </div>
-          {
-            props?.postId &&
-            <div className="modal-action hero-content p-1 mt-3">
-              <label
-                className={BTN_CLASS.primary}
-                htmlFor="NotificationModal"
-                onClick={async () => {
-                  await axios.put('/api/my/notifications/read', data)
-                  await newNotifications()
-                  await router.push(`/tweet/${props.postId}`)
-                }}
-              >
-                View Post
-              </label>
-            </div>
-          }
-          <div className="modal-action hero-content">
-            <label
-              className={BTN_CLASS.primary}
-              htmlFor="NotificationModal"
-              onClick={async () => {
-                await axios.put('/api/my/notifications/read', data)
-                await newNotifications()
-              }}
-            >
-              Close
-            </label>
           </div>
         </div>
-      </div>
-    </div>
+      }
+    </>
   )
 }
 
--- src/components/modals/TweetModal.jsx
+++ src/components/modals/TweetModal.jsx
@@ -2,37 +2,45 @@ import NewTweet from '../tweet/NewTweet'
 import TinyRetweet from '../tweet/TinyRetweet'
 import CloseBtn from '../buttons/CloseBtn'
 
+import useProfile from '../../hooks/useProfile'
+
 const TweetModal = (props) => {
+  const { profile } = useProfile()
 
   return(
     <>
-      <input className="modal-toggle" type="checkbox" id="TweetModal" />
-      <div className="modal overflow-y-auto" htmlFor="TweetModal">
-        <label className="flex flex-col bg-zinc-1000 border border-zinc-700 shadow-sm shadow-zinc-500 rounded-md w-1/2">
-          <div className="modal-action">
-            <label className="btn hover:bg-zinc-800 btn btn-sm btn-circle mx-4 my-2 text-2xl text-zinc-0" htmlFor="TweetModal">
-              <CloseBtn />
+      {
+        profile?.id &&
+        <div>
+          <input className="modal-toggle" type="checkbox" id="TweetModal" />
+          <div className="modal overflow-y-auto" htmlFor="TweetModal">
+            <label className="flex flex-col bg-zinc-1000 border border-zinc-700 shadow-sm shadow-zinc-500 rounded-md w-1/2">
+              <div className="modal-action">
+                <label className="btn hover:bg-zinc-800 btn btn-sm btn-circle mx-4 my-2 text-2xl text-zinc-0" htmlFor="TweetModal">
+                  <CloseBtn />
+                </label>
+              </div>
+              {
+                props.type === 'REPLY' &&
+                <div className="px-6">
+                  <TinyRetweet postId={props.post.id} reply={true}/>
+                  <div className="flex flex-row px-7 py-2">
+                    <div className="text-zinc-500">
+                      Replying to
+                    </div>
+                    <div className="text-sky-600 pl-1">
+                      @{props.user.userName}
+                    </div>
+                  </div>
+                </div>
+              }
+              <label className="modal-box">
+                <NewTweet postType={props.type} post={props.post} user={props.user} />
+              </label>
             </label>
           </div>
-          {
-            props.type === 'REPLY' &&
-            <div className="px-6">
-              <TinyRetweet postId={props.post.id} reply={true}/>
-              <div className="flex flex-row px-7 py-2">
-                <div className="text-zinc-500">
-                  Replying to
-                </div>
-                <div className="text-sky-600 pl-1">
-                  @{props.user.userName}
-                </div>
-              </div>
-            </div>
-          }
-          <label className="modal-box">
-            <NewTweet postType={props.type} post={props.post} user={props.user} />
-          </label>
-        </label>
-      </div>
+        </div>
+      }
     </>
   )
 }
