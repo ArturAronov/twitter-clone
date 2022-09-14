04bed908d072d5bace2eb6c56fdf804ae43e485e
ArturAronov <unionco@icloud.com>
2022-09-14 16:18:21 +0800

    bug fixes

--- src/pages/bookmarks/index.jsx
+++ src/pages/bookmarks/index.jsx
@@ -1,13 +1,18 @@
+import {useState, useEffect} from 'react'
 import useBookmarks from '../../hooks/useBookmarks'
 import TweetFull from '../../components/tweet/TweetFull'
 
 const Bookmarks = () => {
+  const [bookmark, setBookmark] = useState([])
   const { bookmarks } = useBookmarks()
 
+  useEffect(() => {
+    setBookmark(bookmarks)
+  }, [bookmarks])
   return(
     <>
-      { bookmarks &&
-        bookmarks.map(element =>{
+      { bookmark.length>0 &&
+        bookmark.map(element =>{
           return (
             <div key={element.id}>
               <TweetFull tweet={element.post} />
